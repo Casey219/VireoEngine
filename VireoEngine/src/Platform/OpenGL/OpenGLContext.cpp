254bea228768b8e17a7a15895e73a580412fedf4
casey219 <2414996036@qq.com>
2026-01-20 19:06:16 +0800

    first triangle and untracked imgui.ini

--- VireoEngine/src/Platform/OpenGL/OpenGLContext.cpp
+++ VireoEngine/src/Platform/OpenGL/OpenGLContext.cpp
@@ -1,10 +1,10 @@
 #include<virpch.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include<Logger.h>
 #include <Core.h>
 #include "OpenGLContext.h"
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
 
 
 namespace Vireo {
@@ -20,6 +20,9 @@ namespace Vireo {
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		VIR_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		VIR_CORE_INFO("OpenGL Renderer: {0}", (const char*)glGetString(GL_RENDERER));
+		VIR_CORE_INFO("OpenGL Version: {0}", (const char*)glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
