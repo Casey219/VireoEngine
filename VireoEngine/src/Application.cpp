2915741567738edade72f368e837616041a543de
casey219 <2414996036@qq.com>
2026-01-22 19:13:57 +0800

    added buffer layout descriptions for vertex buffers

--- VireoEngine/src/Application.cpp
+++ VireoEngine/src/Application.cpp
@@ -14,6 +14,30 @@
 namespace Vireo {
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case Vireo::ShaderDataType::Float:    return GL_FLOAT;
+		case Vireo::ShaderDataType::Float2:   return GL_FLOAT;
+		case Vireo::ShaderDataType::Float3:   return GL_FLOAT;
+		case Vireo::ShaderDataType::Float4:   return GL_FLOAT;
+		case Vireo::ShaderDataType::Mat3:     return GL_FLOAT;
+		case Vireo::ShaderDataType::Mat4:     return GL_FLOAT;
+		case Vireo::ShaderDataType::Int:      return GL_INT;
+		case Vireo::ShaderDataType::Int2:     return GL_INT;
+		case Vireo::ShaderDataType::Int3:     return GL_INT;
+		case Vireo::ShaderDataType::Int4:     return GL_INT;
+		case Vireo::ShaderDataType::Bool:     return GL_BOOL;
+		}
+
+		VIR_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
+
+
+
 	Application::Application() {
 		VIR_CORE_ASSERT(!s_Instance, "Application already exists!");
 		s_Instance = this;
@@ -29,19 +53,41 @@ namespace Vireo {
 		glBindVertexArray(m_VertexArray);
 
 		
-
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
+
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-	
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		/*glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);*/
+
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index,
+				element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
+
 
 		uint32_t indices[3] = { 0, 1, 2 };
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
@@ -50,12 +96,15 @@ namespace Vireo {
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 
 			out vec3 v_Position;
+			out vec4 v_Color;
 
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);	
 			}
 		)";
@@ -66,10 +115,11 @@ namespace Vireo {
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
-
+			in vec4 v_Color;
 			void main()
 			{
-				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				//color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
