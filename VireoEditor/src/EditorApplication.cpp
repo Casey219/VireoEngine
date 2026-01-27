87786ce5d019944b0febcdce3b5fed439d49f9a2
casey219 <2414996036@qq.com>
2026-01-27 18:43:16 +0800

    added blending and supports for RGBA Texture

--- VireoEditor/src/EditorApplication.cpp
+++ VireoEditor/src/EditorApplication.cpp
@@ -164,7 +164,9 @@ public:
 
 		m_TextureShader.reset(Vireo::Shader::Create(textureShaderVertexSrc, textureShaderFragmentSrc));
 
+
 		m_Texture = Vireo::Texture2D::Create("assets/textures/Checkerboard.png");
+		m_ChernoLogoTexture = Vireo::Texture2D::Create("assets/textures/ChernoLogo.png");
 
 		std::dynamic_pointer_cast<Vireo::OpenGLShader>(m_TextureShader)->Bind();
 		std::dynamic_pointer_cast<Vireo::OpenGLShader>(m_TextureShader)->UploadUniformInt("u_Texture", 0);
@@ -218,6 +220,9 @@ public:
 		m_Texture->Bind();
 		Vireo::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 
+		m_ChernoLogoTexture->Bind();
+		Vireo::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+
 		// Triangle
 		// Hazel::Renderer::Submit(m_Shader, m_VertexArray);
 
@@ -243,7 +248,7 @@ private:
 
 	Vireo::Ref<Vireo::Shader> m_FlatColorShader,m_TextureShader;
 	Vireo::Ref<Vireo::VertexArray> m_SquareVA;
-	Vireo::Ref<Vireo::Texture2D> m_Texture;
+	Vireo::Ref<Vireo::Texture2D> m_Texture,m_ChernoLogoTexture;
 
 	Vireo::OrthographicCamera m_Camera;
 	glm::vec3 m_CameraPosition;
