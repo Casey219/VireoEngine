b1080d93a6effd6b29d28106f0ce63c288cd45b6
casey219 <2414996036@qq.com>
2026-02-19 10:42:01 +0800

    Content Browser panel thumbnail layout

--- Editor/Panels/ContentBrowserPanel.h
+++ Editor/Panels/ContentBrowserPanel.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <filesystem>
-
+#include "Renderer/Texture.h"
 
 namespace Vireo {
 	class ContentBrowserPanel
@@ -11,5 +11,8 @@ namespace Vireo {
 		void OnImGuiRender();
 	private:
 		std::filesystem::path m_CurrentDirectory;
+
+		Ref<Texture2D> m_DirectoryIcon;
+		Ref<Texture2D> m_FileIcon;
 	};
 }
